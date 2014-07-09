0ea33e4104f393f7f6a989198672a580dedb2ee3
teaklvs <tea.kolevska@gmail.com>
2014-07-09 12:01:21 +0200

    socket address

--- server_socket.py
+++ server_socket.py
@@ -36,7 +36,7 @@ from threading import Thread
 def main(argv):  
     running_threads={}   
     print ("In main before while, running threads: %s" %running_threads) 
-    HOST = '127.0.0.1'   # Symbolic name meaning all available interfaces
+    HOST = '160.85.4.236'   # Symbolic name meaning all available interfaces
     PORT = 9005 # Arbitrary non-privileged port 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     print("Socket created.")
